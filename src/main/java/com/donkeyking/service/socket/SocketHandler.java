c122cd932ae947240e9d6ba4476dfb6ae43a7af3
Joby Mathews <joby.mathews@oracle.com>
2022-11-02 20:01:23 +0530

    added git ignore file

--- src/main/java/com/donkeyking/service/socket/SocketHandler.java
+++ src/main/java/com/donkeyking/service/socket/SocketHandler.java
@@ -104,8 +104,8 @@ public class SocketHandler extends TextWebSocketHandler {
                     this.submitCard(message);
                     break;
                 case MSG_START_GAME:
-                    //String[] cards= new String[]{"AC","AD","AH","AS","2C","2D","2H","2S","3C","3D","3H","3S","4C","4D","4H","4S","5C","5D","5H","5S","6C","6D","6H","6S","7C","7D","7H","7S","8C","8D","8H","8S","9C","9D","9H","9S","10C","10D","10H","10S","JC","JD","JH","JS","QC","QD","QH","QS","KC","KD","KH","KS"};
-                    String[] cards= new String[]{"AC","AD","AH","AS","2C","2D","2H","2S","3C"};
+                    String[] cards= new String[]{"AC","AD","AH","AS","2C","2D","2H","2S","3C","3D","3H","3S","4C","4D","4H","4S","5C","5D","5H","5S","6C","6D","6H","6S","7C","7D","7H","7S","8C","8D","8H","8S","9C","9D","9H","9S","10C","10D","10H","10S","JC","JD","JH","JS","QC","QD","QH","QS","KC","KD","KH","KS"};
+                    //String[] cards= new String[]{"AC","AD","AH","AS","2C","2D","2H","2S","3C"};
                     List<String> shuffledCards = Arrays.asList(cards);
                     Collections.shuffle(shuffledCards);
                     room = userIdToRoomMap.get(message.getFrom()).get();
